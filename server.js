9809d89e95bcb19113fd36abdc66e2dec752c22e
Fox <michael_topfox@hotmail.com>
2021-11-02 04:15:04 +0200

    V4

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-let projectData ;
+let projectData;
 
 // include Express to run server and routes
 const express = require("express");
@@ -22,7 +22,7 @@ app.use(cors());
 
 app.use(express.static("website"));
 
-const apiKey = "35d1727cc5aa9d0b9dd65e016e8b7f61";
+// const apiKey = "35d1727cc5aa9d0b9dd65e016e8b7f61";
 
 //port for express server
 const port = process.env.PORT || 3000;
@@ -35,44 +35,31 @@ function listening() {
   console.log("server running on localhost port: ", port);
 }
 
-let zip ;
-let feel ;
-let date ;
+let zip;
+let feel;
+let date;
 let data;
+let wData;
 
-app.post('/getData', getData);
+app.post("/getData", getData);
 function getData(req, res) {
-zip = req.body.zip;
-date = req.body.date;
-feel = req.body.feel;
- 
-  data = { zip:` ${ zip }`, date:date, feel:feel };
- 
+  wData = req.body.weather;
+  zip = req.body.zip;
+  // console.log(wData);
+  date = req.body.date;
+  feel = req.body.feel;
+  data = { zip: ` ${zip}`, date: date, feel: feel };
+  projectData = { wData, data };
+  // console.log(projectData);
 }
 
-
 // Initialize all route with a callback function
-app.get('/all', sendData)
-
+app.get("/all", sendData);
 // Callback function to complete GET '/all'
 function sendData(req, res) {
-  
-  request(
-    `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=metric&appid=${apiKey}`,
-    function (error, response, body) {
-      
-      projectData = JSON.parse(body);
-      projectData.user = data
-
-      if (response.statusCode === 200) {
-        
-         res.status(200).send(projectData);
-       } else (response.statusCode === 400); {
-            res.status(400).send();
-          }
-    }
-  );
-
+  // projectData = JSON.parse(body);
+  res.status(200).send(projectData);
+  res.status(400).send();
 }
 
 // not found 404
--- website/app.js
+++ website/app.js
@@ -1,13 +1,52 @@
 /* Global Variables */
+const apiKey = "35d1727cc5aa9d0b9dd65e016e8b7f61";
 let date = document.getElementById("date");
 let temp = document.getElementById("temp");
 let content = document.getElementById("content");
 let city = document.getElementById("city");
 let description = document.getElementById("desc");
+let weatherData;
 // Create a new date instance dynamically with JS
 
 let d = new Date();
 let newDate = d.getMonth() + "/" + d.getDate() + "/" + d.getFullYear();
+
+/* Function to GET weather Data */
+const openWeather = async (url) => {
+  const request = await fetch(url);
+  try {
+    // Transform into JSON
+    weatherData = await request.json();
+    // console.log(weatherData, "all DATA");
+    // Write updated data to DOM elements
+  } catch (error) {
+    console.log("error", error);
+    // if we catch an error warn the user that the city was not found & update DOM elements.
+    // appropriately handle the error
+  }
+};
+
+/* Function to POST data */
+const postData = async (url = "", data = []) => {
+  // console.log(data, "data");
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+
+  try {
+    const newData = response;
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
 
 /* Function to GET Project Data */
 const retrieveData = async () => {
@@ -18,16 +57,16 @@ const retrieveData = async () => {
     console.log(allData, "all DATA");
     // Write updated data to DOM elements
     document.getElementById("temp").innerHTML =
-      Math.round(allData.main.temp) + "°C";
-    temp.style = '';
-    document.getElementById("content").innerHTML = allData.user.feel;
-    document.getElementById("date").innerHTML = allData.user.date;
-    let icon = allData.weather[0].icon;
+      Math.round(allData.wData.main.temp) + "°C";
+    temp.style = "";
+    document.getElementById("content").innerHTML = "you're feeling : " + allData.data.feel;
+    document.getElementById("date").innerHTML = allData.data.date;
+    let icon = allData.wData.weather[0].icon;
     document.getElementById(
       "pic"
     ).src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-    city.innerHTML = `City : ${allData.name}`;
-    description.innerHTML = allData.weather[0].description;
+    city.innerHTML = `City : ${allData.wData.name}`;
+    description.innerHTML = allData.wData.weather[0].description;
   } catch (error) {
     console.log("error", error);
     // if we catch an error warn the user that the city was not found & update DOM elements.
@@ -42,33 +81,24 @@ const retrieveData = async () => {
   }
 };
 
-/* Function to POST data */
-const postData = async (url = "", data = []) => {
-  console.log(data, "data");
-  const response = await fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  });
-
-  try {
-    const newData = await response.json();
-    
-    return newData;
-  } catch (error) {
-    console.log("error", error);
-    // appropriately handle the error
-  }
-};
 // get zip on click
-document.getElementById("generate").addEventListener("click", action);
+document.getElementById("generate").addEventListener("click", () => action())
 
-function action() {
+
+async function action() {
   let zipCode = document.getElementById("zip").value;
   let feelings = document.getElementById("feelings").value;
-  postData("/getData", { zip: zipCode, date: newDate, feel: feelings });
+  const baseURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=metric&appid=${apiKey}`;
+  await openWeather(baseURL);
+  try {
+    postData("/getData", {
+      weather: weatherData,
+      zip: zipCode,
+      date: newDate,
+      feel: feelings,
+    })
+  } catch (error) {};
   retrieveData();
-}
+ };
+  
+
